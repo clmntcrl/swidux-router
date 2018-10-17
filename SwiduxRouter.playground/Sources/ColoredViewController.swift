4b43179c2e9cdb8376b9003f340a135c0a8b5ba4
clément cyril <cyril@clmntcrl.io>
2018-10-17 19:08:44 +0200

    Improve code

--- SwiduxRouter.playground/Sources/ColoredViewController.swift
+++ SwiduxRouter.playground/Sources/ColoredViewController.swift
@@ -13,84 +13,62 @@ public final class ColoredViewController: ParametricRoutableViewController {
     }
 
     var label: UILabel!
-    var subscription: Any!
+    var subscription = [Any]()
 
     public override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.frame = store.state.liveViewFrame
+        // Build view
 
+        let container = UIStackView()
+        view.addSubview(container)
+        container.axis = .vertical
+        container.translatesAutoresizingMaskIntoConstraints = false
+        [ NSLayoutConstraint.Attribute.leading, .top, .trailing, .bottom ]
+            .map { NSLayoutConstraint(item: container, attribute: $0, relatedBy: .equal, toItem: view, attribute: $0, multiplier: 1, constant: 0) }
+            .forEach { $0.isActive = true }
 
         label = UILabel()
-        let maxLabelFrame = CGRect(
-            x: 16,
-            y: 16,
-            width: view.frame.width - 32,
-            height: view.frame.height - 32
-        )
-        label.frame = maxLabelFrame
-        label.font = .systemFont(ofSize: 14)
+        label.textColor = .white
         label.numberOfLines = 0
-        view.addSubview(label)
+        label.font = .systemFont(ofSize: 14)
+        container.addArrangedSubview(label)
 
-        subscription = store.subscribe(\AppState.routes) { [weak self] routes in
-            self.flatMap {
-                $0.label.text = routes
-                    .map { "\($0)" }
-                    .joined(separator: "\n → ")
-                $0.label.frame.size = $0.label.sizeThatFits(maxLabelFrame.size)
+        let buttons = UIStackView(arrangedSubviews:[
+            buildButton(title: "BACK", action: #selector(goBack)),
+            buildButton(title: "PUSH", action: #selector(pushColoredViewController)),
+            buildButton(title: "RESET", action: #selector(resetRouterWithRandomRouteStack)),
+        ])
+        buttons.distribution = .fillEqually
+        container.addArrangedSubview(buttons)
+
+        // Subscribe store changes
+
+        subscription = [
+            store.subscribe(\.liveViewFrame) { [weak self] frame in self?.view.frame = frame },
+            store.subscribe(\AppState.routes) { [weak self] routes in
+                self.flatMap {
+                    $0.label.text = "\troutes: [\n\t\t"
+                        + routes
+                            .map { "\($0.type)(params: 0x\(String($0.params!.value as! Int32, radix: 16)))" }
+                            .joined(separator: ",\n\t\t")
+                        + ",\n\t]"
+                }
             }
-        }
-
-        let stack = UIStackView()
-        stack.frame.size.width = view.frame.width
-        stack.frame.size.height = 64
-        stack.center = view.center
-        stack.distribution = .fillEqually
-        view.addSubview(stack)
-
-        let reset = UIButton(type: .system)
-        reset.setTitle("RESET", for: .normal)
-        reset.setTitleColor(.white, for: .normal)
-        reset.titleLabel?.font = .boldSystemFont(ofSize: 32)
-        reset.backgroundColor = UIColor.white.withAlphaComponent(0.3)
-        reset.addTarget(self, action: #selector(resetRouter), for: .touchUpInside)
-        stack.addArrangedSubview(reset)
-
-        let back = UIButton(type: .system)
-        back.setTitle("BACK", for: .normal)
-        back.setTitleColor(.white, for: .normal)
-        back.titleLabel?.font = .boldSystemFont(ofSize: 32)
-        back.backgroundColor = UIColor.white.withAlphaComponent(0.3)
-        back.addTarget(self, action: #selector(goBack), for: .touchUpInside)
-        stack.addArrangedSubview(back)
-
-        let push = UIButton(type: .system)
-        push.setTitle("PUSH", for: .normal)
-        push.setTitleColor(.white, for: .normal)
-        push.titleLabel?.font = .boldSystemFont(ofSize: 32)
-        push.backgroundColor = UIColor.white.withAlphaComponent(0.3)
-        push.addTarget(self, action: #selector(pushColoredViewController), for: .touchUpInside)
-        stack.addArrangedSubview(push)
+        ]
     }
 
-    private func uicolor(hex: Int32) -> UIColor {
-        return UIColor(
-            red: CGFloat((hex >> 16) & 0xff) / 255.0,
-            green: CGFloat((hex >> 8) & 0xff) / 255.0,
-            blue: CGFloat(hex & 0xff) / 255.0,
-            alpha: 1
-        )
+    private func buildButton(title: String, action: Selector) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 20)
+        button.backgroundColor = UIColor.white.withAlphaComponent(0.3)
+        button.addTarget(self, action: action, for: .touchUpInside)
+        return button
     }
 
-    @objc private func resetRouter() {
-        store.dispatch(
-            RouteAction.reset(
-                routes: Array(repeating: (), count: Int.random(in: 1...5))
-                    .map { _ in  RootRoute.coloredRoute() }
-            )
-        )
-    }
+    // MARK: - Actions
 
     @objc private func goBack() {
         store.dispatch(RouteAction.back)
@@ -99,6 +77,26 @@ public final class ColoredViewController: ParametricRoutableViewController {
     @objc private func pushColoredViewController() {
         store.dispatch(
             RouteAction.push(route: RootRoute.coloredRoute())
+        )
+    }
+
+    @objc private func resetRouterWithRandomRouteStack() {
+        store.dispatch(
+            RouteAction.reset(
+                routes: Array(repeating: (), count: Int.random(in: 1...5))
+                    .map { _ in  RootRoute.coloredRoute() }
+            )
+        )
+    }
+
+    // MARK: - UIColor from hex helper
+
+    private func uicolor(hex: Int32) -> UIColor {
+        return UIColor(
+            red: CGFloat((hex >> 16) & 0xff) / 255.0,
+            green: CGFloat((hex >> 8) & 0xff) / 255.0,
+            blue: CGFloat(hex & 0xff) / 255.0,
+            alpha: 1
         )
     }
 }
