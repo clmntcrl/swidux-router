238ac3684dac2b8f488e92a458a752505ae02cd4
clément cyril <cyril@clmntcrl.io>
2019-01-22 19:40:20 +0100

    Use git submodule and remove installation section and SwiftPM support because it needs more work

---
+++ Tests/SwiduxRouterTests.swift
@@ -0,0 +1,7 @@
+//  Created by Cyril Clément
+//  Copyright © 2019 clmntcrl. All rights reserved.
+
+import XCTest
+@testable import SwiduxRouter
+
+class SwiduxRouterTests: XCTestCase {}
--- Tests/SwiduxRouterTests/SwiduxRouterTests.swift
+++
@@ -1,5 +0,0 @@
-import XCTest
-@testable import SwiduxRouter
-
-final class SwiduxRouterTests: XCTestCase {
-}
